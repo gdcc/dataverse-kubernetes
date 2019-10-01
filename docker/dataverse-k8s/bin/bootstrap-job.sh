090f158cd6664e407a67710200e0dece13eb6e31
Oliver Bertuch <o.bertuch@fz-juelich.de>
2019-10-01 14:54:12 +0200

    Refactor jobs: * Use env vars for service location to be more flexible * Add `-sS` to every curl call to be more quiet. Relates to #93

--- docker/dataverse-k8s/bin/bootstrap-job.sh
+++ docker/dataverse-k8s/bin/bootstrap-job.sh
@@ -8,10 +8,12 @@
 ################################################################################
 
 # Fail on any error
-set -e
+set -euo pipefail
 # Include some sane defaults
 . ${SCRIPT_DIR}/default.config
-DATAVERSE_K8S_HOST=${DATAVERSE_K8S_HOST:-${DATAVERSE_SERVICE_HOST}}
+DATAVERSE_SERVICE_HOST=${DATAVERSE_SERVICE_HOST:-"dataverse"}
+DATAVERSE_SERVICE_PORT=${DATAVERSE_SERVICE_PORT:-"8080"}
+DATAVERSE_URL=${DATAVERSE_URL:-"http://${DATAVERSE_SERVICE_HOST}:${DATAVERSE_SERVICE_PORT}"}
 SOLR_K8S_HOST=${SOLR_K8S_HOST:-${SOLR_SERVICE_HOST}}
 
 # Check postgres and API key secrets are available
@@ -34,7 +36,7 @@ psql -h ${POSTGRES_SERVER} -U ${POSTGRES_USER} ${POSTGRES_DATABASE} < ${HOME_DIR
 # 2) Initialize common data structures to make Dataverse usable
 cd ${HOME_DIR}/dvinstall
 # 2a) Patch load scripts with k8s based URL
-sed -i -e "s#localhost:8080#${DATAVERSE_K8S_HOST}:8080#" setup-*.sh
+sed -i -e "s#localhost:8080#${DATAVERSE_SERVICE_HOST}:${DATAVERSE_SERVICE_PORT}#" setup-*.sh
 # 2b) Patch user and root dataverse JSON with contact email
 sed -i -e "s#root@mailinator.com#${CONTACT_MAIL}#" data/dv-root.json
 sed -i -e "s#dataverse@mailinator.com#${CONTACT_MAIL}#" data/user-admin.json
@@ -42,13 +44,13 @@ sed -i -e "s#dataverse@mailinator.com#${CONTACT_MAIL}#" data/user-admin.json
 ./setup-all.sh --insecure
 
 # 4.) Configure Solr location
-curl -X PUT -d "${SOLR_K8S_HOST}:8983" "http://${DATAVERSE_K8S_HOST}:8080/api/admin/settings/:SolrHostColonPort"
+curl -sS -X PUT -d "${SOLR_K8S_HOST}:8983" "${DATAVERSE_URL}/api/admin/settings/:SolrHostColonPort"
 
 # 5.) Configure system email (otherwise no email will be send)
-curl -X PUT -d "${ADMIN_MAIL}" "http://${DATAVERSE_K8S_HOST}:8080/api/admin/settings/:SystemEmail"
+curl -sS -X PUT -d "${ADMIN_MAIL}" "${DATAVERSE_URL}/api/admin/settings/:SystemEmail"
 
 # 6.) Block access to the API endpoints, but allow for request with key from secret
-curl -X DELETE "http://${DATAVERSE_K8S_HOST}:8080/api/admin/settings/BuiltinUsers.KEY"
-curl -X PUT -d "`cat ${SECRETS_DIR}/api/key`" "http://${DATAVERSE_K8S_HOST}:8080/api/admin/settings/:BlockedApiKey"
-curl -X PUT -d unblock-key "http://${DATAVERSE_K8S_HOST}:8080/api/admin/settings/:BlockedApiPolicy"
-curl -X PUT -d admin,test "http://${DATAVERSE_K8S_HOST}:8080/api/admin/settings/:BlockedApiEndpoints"
+curl -sS -X DELETE "${DATAVERSE_URL}/api/admin/settings/BuiltinUsers.KEY"
+curl -sS -X PUT -d "`cat ${SECRETS_DIR}/api/key`" "${DATAVERSE_URL}/api/admin/settings/:BlockedApiKey"
+curl -sS -X PUT -d unblock-key "${DATAVERSE_URL}/api/admin/settings/:BlockedApiPolicy"
+curl -sS -X PUT -d admin,test "${DATAVERSE_URL}/api/admin/settings/:BlockedApiEndpoints"
