9696b896eb6655493d2e53f679539ff4cf5dfab4
Oliver Bertuch <o.bertuch@fz-juelich.de>
2019-10-23 12:16:15 +0200

    Add kustomize 2.0.3 for linting (same version as in kubectl >1.14)

--- test/kubeval/prepare.sh
+++ test/kubeval/prepare.sh
@@ -14,3 +14,17 @@ if [ ! -x "${KUBEVAL_DIR}/kubeval" ] || [ ! -f "${KUBEVAL_DIR}/kubeval-${KUBEVAL
   wget -q -O "${KUBEVAL_DIR}/kubeval-${KUBEVAL_RELEASE}.tar.gz" "${KUBEVAL_URL}"
   tar xf "${KUBEVAL_DIR}/kubeval-${KUBEVAL_RELEASE}.tar.gz" -C "${KUBEVAL_DIR}" kubeval
 fi
+
+KUSTOMIZE_RELEASE=${KUSTOMIZE_RELEASE:-"2.0.3"}
+KUSTOMIZE_PLATFORM=${KUSTOMIZE_PLATFORM:-"linux_amd64"}
+KUSTOMIZE_URL="https://github.com/kubernetes-sigs/kustomize/releases/download/v${KUSTOMIZE_RELEASE}/kustomize_${KUSTOMIZE_RELEASE}_${KUSTOMIZE_PLATFORM}"
+
+if [ ! -f "${KUBEVAL_DIR}/kustomize_${KUSTOMIZE_RELEASE}" ] || [ ! -x "${KUBEVAL_DIR}/kustomize" ]; then
+  echo Downloading and extracting kustomize-${KUSTOMIZE_RELEASE}... Please be patient.
+  # delete old releases
+  rm -f "${KUBEVAL_DIR}/kustomize*"
+  # download new
+  wget -q -O "${KUBEVAL_DIR}/kustomize_${KUSTOMIZE_RELEASE}" "${KUSTOMIZE_URL}"
+  chmod +x "${KUBEVAL_DIR}/kustomize_${KUSTOMIZE_RELEASE}"
+  ln -s kustomize_${KUSTOMIZE_RELEASE} ${KUBEVAL_DIR}/kustomize
+fi
