51502d4a70314539bea181c28a5ddd77aaab4468
Oliver Bertuch <o.bertuch@fz-juelich.de>
2019-10-28 13:48:57 +0100

    Add custom CSS styling for footnote references (make them appear in superscript)

--- docs/conf.py
+++ docs/conf.py
@@ -142,7 +142,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['.static']
+html_static_path = ['.static']
 
 html_favicon = "images/favicon.png"
 html_logo = "images/dataverse-bot-think-k8s.png"
@@ -159,6 +159,11 @@ html_logo = "images/dataverse-bot-think-k8s.png"
 html_sidebars = {
    '**': ['globaltoc.html']
 }
+
+# Add custom CSS styling
+html_css_files = [
+    'css/custom.css',
+]
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
